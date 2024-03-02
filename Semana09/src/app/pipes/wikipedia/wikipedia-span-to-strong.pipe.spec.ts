e3da973a0da556b780f029bb811c4c672166d0a6
nairansantos <nairanb28@gmail.com>
2024-03-02 16:15:35 -0300

    nomeando as pastas

--- Semana09/src/app/pipes/wikipedia/wikipedia-span-to-strong.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WikipediaSpanToStrongPipe } from './wikipedia-span-to-strong.pipe';
-
-describe('WikipediaSpanToStrongPipe', () => {
-  it('create an instance', () => {
-    const pipe = new WikipediaSpanToStrongPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
